5a29a0e6424c7771a85d02683c1659d77353ee6d
WindowsVistaisCool <kwrush024@pccsk12.com>
2021-09-06 20:58:42 -0400

    first version

---
+++ main.py
@@ -0,0 +1,44 @@
+import random
+import discord
+from discord.ext import commands
+from cogs.util import store, ready_status
+
+client = commands.Bot(command_prefix=store('config.json', 'pfx', True))
+
+@client.event
+async def on_ready():
+    await ready_status(client, store('config.json', None, True))
+    print("Ready")
+
+@client.event
+async def on_command_error(ctx, error):
+    await ctx.send(f"ERROR: {error}")
+
+@client.event
+async def on_message(message):
+    if message.author.bot: return
+    # if message.channel.id == 0:
+    await client.process_commands(message)
+
+@client.command()
+async def wakeupmrwest(ctx):
+    await ctx.send("<:horny:815833288936914944> WAKE UP MR WEST <:horny:815833288936914944>")
+
+@client.command()
+async def quote(ctx):
+    await ctx.send(random.choice(store('config.json', 'kanye', True)['quotes']))
+
+@client.command()
+async def addq(ctx, *, request):
+    c = client.get_channel(868667030973321256)
+    await c.send(request)
+    await ctx.send("ok your request has been sent")
+
+@client.command()
+@commands.has_any_role(872954220884688937, 831577493080113193, 792875711676940321, 788912937481273344, 788911513129058304)
+async def accept(ctx, id):
+    x = store('config.json', 'kanye', True)
+    message = await ctx.channel.fetch_message(int(id))
+    x['quotes'].append(message.content)
+
+client.run(store('config.json', 'token', True))
